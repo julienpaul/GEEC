9325469a105e203bdab29115f10b51e2e87da15b
Julien Paul <julien.paul@umontpellier.fr>
2023-06-29 10:43:21 +0200

    Change to do not run mypy on github-action
    Change to test only successful run (not timing) on test_geec.py

--- tests/test_geec.py
+++ tests/test_geec.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # test_geec.py
 
-# import timeit
-
 from typer.testing import CliRunner
 
 from geec import __version__
@@ -28,20 +26,11 @@ def test_grad():
 
 def test_geec_cli_grav():
     runner = CliRunner()
-    # start_time = time.time()
     result = runner.invoke(app, "test-grav")
-    # elapse = time.time() - start_time
     assert result.exit_code == 0
-    # assert elapse <= 0.5  # sec
-    # elapse = 1000 * timeit.timeit("test_grav()", setup=setup, number=100) / 100
-    # assert elapse <= 500  # usec/pass
 
 
 def test_geec_cli_grad():
     runner = CliRunner()
-    # start_time = time.time()
     result = runner.invoke(app, "test-grad")
-    # elapse = time.time() - start_time
     assert result.exit_code == 0
-    # elapse =1000 * timeit.timeit("test_grad()", setup=setup, number=100) / 100
-    # assert elapse <= 1000  # usec/pass
